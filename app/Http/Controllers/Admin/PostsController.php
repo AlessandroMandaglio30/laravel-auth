3f4b4a1756ba84d5ed73198ae0232b91564301ed
Alessandro Mandaglio <ale.manda.30@gmail.com>
2022-02-16 15:46:13 +0100

    Modifiche e fine CRUD

--- app/Http/Controllers/Admin/PostsController.php
+++ app/Http/Controllers/Admin/PostsController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Post;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Str;
 use function GuzzleHttp\Promise\all;
 
 class PostsController extends Controller
 {
+    protected $validationRule = [
+        "title" => "required|string|max:120",
+        "content" => "required",
+        "published" => "sometimes|accepted"
+    ];
     /**
      * Display a listing of the resource.
      *
@@ -29,7 +34,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -40,7 +45,30 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validazione dei dati
+        $request->validate($this->validationRule);
+        //creazione del post
+        $data = $request->all();
+
+        $newPost = new Post();
+        $newPost->title = $data["title"];
+        $newPost->content = $data["content"];
+        $newPost->published = isset($data["published"]);
+
+        $slug = Str::of($newPost->title)->slug("-");
+        $count = 1;
+
+        while (Post::where("slug", $slug)->first()) {
+            $slug = Str::of($newPost->title)->slug("-") . "-{$count}";
+            $count++;
+        }
+
+        $newPost->slug = $slug;
+
+        $newPost->save();
+        //redirect al post 
+
+        return redirect()->route("posts.show", $newPost->id);
     }
 
     /**
@@ -63,9 +91,9 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        return view("admin.posts.edit", compact("post"));
     }
 
     /**
@@ -75,9 +103,39 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $request->validate($this->validationRule);
+
+        //aggiorno il post
+        $data = $request->all();
+
+        if ($post->title != $data['title']) {
+            $post->title = $data['title'];
+
+            $slug = Str::of($post->title)->slug("-");
+
+            if ($slug != $post->slug) {
+                $count = 1;
+
+                while (Post::where("slug", $slug)->first()) {
+                    $slug = Str::of($post->title)->slug("-") . "-{$count}";
+                    $count++;
+                }
+
+                $post->slug = $slug;
+            }
+        }
+
+
+        $post->content = $data["content"];
+
+        $post->published = isset($data["published"]);
+
+
+        $post->save();
+
+        return redirect()->route("posts.show", $post->id);
     }
 
     /**
@@ -86,8 +144,10 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect()->route("posts.index");
     }
 }
